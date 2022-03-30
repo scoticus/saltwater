8be3620a4e649f62bb9ece64562fced325f87f3a
Scoticus <scoticus@users.noreply.github.com>
2022-03-30 13:46:46 +0100

    add Handlebars templating

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 #[macro_use] extern crate rocket;
 
+use rocket_dyn_templates::Template;
+use serde::{Serialize};
+
+#[derive(Serialize)]
+struct Context {
+  title: &'static str
+}
+
 #[get("/")]
-fn index() -> &'static str {
-  "Hello, world!"
+fn index() -> Template {
+    Template::render("index", &Context { title: "Hello, World!" })
 }
 
 #[get("/login")]
-fn login() -> &'static str {
-  "Login page"
+fn login() -> Template {
+  Template::render("index", &Context { title: "Login page" })
 }
 
 #[launch]
 fn rocket() -> _ {
-  rocket::build().mount("/", routes![index, login])
+  rocket::build().mount("/", routes![index, login]).attach(Template::fairing())
 }
